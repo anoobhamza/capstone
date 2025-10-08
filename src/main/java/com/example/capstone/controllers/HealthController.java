68a9201fe1c600c416049a5370ec73032a0eaa6e
anoobhamza@gmail.com <anoobhamza@gmail.com>
2025-10-08 21:01:00 +0530

    first commit

---
+++ src/main/java/com/example/capstone/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package com.example.capstone.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/health")
+    public String hello(){
+        return "OK";
+
+    }
+
+}
